f84e99b02c8729b39c17887fe0e6bbedd317664b
Siddhant <patelsiddhant9297@gmail.com>
2024-01-15 09:42:27 +0530

    project structure created for post.

---
+++ src/main/java/com/blog/patelBlog/PatelBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.patelBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatelBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatelBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/patelBlog/PatelBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.patelBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatelBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
